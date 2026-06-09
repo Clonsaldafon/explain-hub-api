905d78676537836ddf2ec5237cece9dd99308394
Soniccooles <andrey.sonic@mail.ru>
2026-06-09 21:32:33 +0500

    Added messages

---
+++ src/Worker/Messages/BaseMessage.cs
@@ -0,0 +1,7 @@
+﻿namespace Worker.Messages;
+
+public abstract class BaseMessage
+{
+    public Guid MessageId { get; set; } = Guid.NewGuid();
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
---
+++ src/Worker/Messages/LikeNotificationMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace Worker.Messages;
+
+public class LikeNotificationMessage : BaseMessage
+{
+    public string Url { get; set; } = string.Empty;
+    public string Recipient { get; set; } =  string.Empty;
+    public string LikerName { get; set; } = string.Empty;
+    public string PostTitle { get; set; } = string.Empty;
+}
